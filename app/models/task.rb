59a8e5796c1ccaff7f74aa8772a47f32042f6eb6
Grandman <root@grandman73.ru>
2014-12-21 22:17:33 +0300

    add some test for task

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,9 +6,19 @@ class Task < ActiveRecord::Base
 
  validates :name, presence: true, length: 3..30
  validates :project, presence: true
+ validates :end_at, presence: true
+ validate :end_at_cannot_be_less_then_created_at
 
  scope :not_completed, -> { where(completed: nil)}
  scope :without_parent, -> { where(parent: nil)}
  scope :find_completed, ->(project) { where(project: project,completed: true)} 
 
+ private
+
+ def end_at_cannot_be_less_then_created_at 
+    return if end_at.blank? || created_at.blank?
+    if end_at < created_at
+      errors.add(:end_at, "can't end earlier than created")
+    end
+ end 
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -6,18 +6,42 @@ class TaskTest < ActiveSupport::TestCase
    end
    test "should not valid without name" do
      @task.name = nil
-     assert !@task.valid?
+     assert !@task.valid?, "task was valid when name is nil "
    end
    test "valid with name length between 3 and 30" do
      @task.name = "Task name"
-     assert @task.valid?
+     assert @task.valid?, "task was not valid when name length between 3 and 30"
    end
-   test "invalid with name length more then 30" do
+   test "invalid with name length more than 30" do
      @task.name = "Task name more then 30 syyyyymbooolllllssss"
-     assert !@task.valid?
+     assert !@task.valid?, "task was valid when task name length more than 30"
    end 
-   test "invalid with name length less then 3" do
+   test "invalid with name length less than 3" do
      @task.name = "1"
+     assert !@task.valid?, "task was valid when task name length less then 3"
+   end
+   test "should return nil if no children" do
+     assert_equal [], @task.children
+   end
+   test "should return tasks if has children" do
+     @task = tasks(:two)
+     assert_equal [tasks(:three)], @task.children
+   end
+   test "should return parent" do
+     @task = tasks(:three)
+     assert_equal tasks(:two), @task.parent
+   end
+   test "should have project" do
+     @task.project = nil
+     assert !@task.valid?, "task was valid when project is nil"
+   end
+
+   test "end_at should not nil" do
+     @task.end_at = nil
+     assert !@task.valid?
+   end
+   test "end_at should not earlier than created_at" do
+     @task = tasks(:two)
      assert !@task.valid?
    end
 end
