689e985c2fdc772493cb6598eddcf38735400b65
Grandman <root@grandman73.ru>
2014-12-22 00:30:15 +0300

    add project validators + tests

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ActiveRecord::Base
  has_many :tasks, dependent: :destroy
+ validates :name, presence: true, length: 6..30
 end
--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class ProjectTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "Project should have name" do
+        project = Project.new(name:"Project name")
+        project2 = Project.new()
+        assert project.valid?
+        assert !project2.valid?
+   end
 end
