6d72e2ed5d5980c8238f55dfe0627794cddb3fe9
Grandman <root@grandman73.ru>
2015-02-11 12:18:36 +0000

    add helper for output subtasks

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,13 @@
 module TasksHelper
+  def print_subtasks (task)
+    result = ""
+    task.children.each do |subtask|
+      result << "<ul><li>#{link_to subtask.name, [@project, subtask]}"
+      if subtask.has_children?
+        result << print_subtasks(subtask)
+      end
+      result << "</li></ul>"
+    end
+    raw(result)
+  end
 end
